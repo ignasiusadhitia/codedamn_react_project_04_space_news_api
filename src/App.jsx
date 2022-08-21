f6a621a9a03a4e9c809fa83cf3c172116c6ce541
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2022-08-21 14:34:16 +0000

    finalized project

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+    const [newsList, setNewsList] = useState([])
+
+    useEffect(() => {
+        fetch("https://api.spaceflightnewsapi.net/v3/articles").then((response) => response.json().then((data) => {
+            setNewsList(data)
+        }))
+    }, [])
+
+	return (
+		<div className="App">
+            <div className="title">
+                <h1>Space News</h1>	
+            </div>            
+            <div className="newsContainer">
+                {newsList.map((value, key) => {
+                    return (
+                        <div key={key} className="article" onClick={() => {window.location.href = value.url}}>
+                            <h3>{value.title}</h3>
+                            <img src={value.imageUrl} alt={value.title} />
+                            <p>{value.summary}</p>
+                            <h4>{value.publishedAt}</h4>
+                        </div>
+                    )
+                })}
+            </div>		
+		</div>
+	)
+}
+
+export default App
